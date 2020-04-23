797c334d4ea36e6c211d979658fa02f8fe26990b
Ross Young <rossmichaelyoung@gmail.com>
2020-04-23 11:56:59 -0500

    refresh rates

--- input/mlx90640_thermal_camera.py
+++ input/mlx90640_thermal_camera.py
@@ -8,6 +8,17 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
+'''
+Refresh Rates
+REFRESH_0_5_HZ
+REFRESH_1_HZ
+REFRESH_2_HZ
+REFRESH_4_HZ
+REFRESH_8_HZ
+REFRESH_16_HZ
+REFRESH_32_HZ
+REFRESH_64_HZ
+'''
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 frame = [0] * 768
